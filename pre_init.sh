c2e1be0b495b961249d51ac0167491defa8588b7
DIVYANSHU SHUKLA <86191568+justmorpheus@users.noreply.github.com>
2023-01-13 01:27:50 +0530

    Update pre_init.sh

--- pre_init.sh
+++ pre_init.sh
@@ -12,9 +12,11 @@ chmod 400 bastion_key.pem
 
 scp -o StrictHostKeyChecking=no -i bastion_key.pem ec2_key.pem ubuntu@$(terraform output bastion_host_public_ip | tr -d '"'):~/
 
+echo "Wait for 6-7 minutes while infra deployment is happening. "
 
+sleep 385
 
-echo "Now ssh into bastion and run the ansible playbook."
+echo "Now ssh into bastion, ansible in also running"
 echo "To ssh: ssh -o StrictHostKeyChecking=no -i bastion_key.pem ubuntu@$(terraform output bastion_host_public_ip | tr -d '"') "
 
 echo "To cleanup run: terraform destroy --auto-approve"

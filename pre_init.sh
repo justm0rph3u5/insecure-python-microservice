4512338e0160aacb3bf3d396b54c9e6eb1a033c5
DIVYANSHU SHUKLA <86191568+justmorpheus@users.noreply.github.com>
2023-01-13 01:39:56 +0530

    Update pre_init.sh

--- pre_init.sh
+++ pre_init.sh
@@ -3,6 +3,8 @@ set +e
 echo "Make sure terraform in installed"
 echo "Current AWS region is set to us-west-2"
 
+alias cdtemp="cd infrastructure/terraform"
+cdtemp
 
 terraform init
 terraform apply --auto-approve
@@ -12,20 +14,10 @@ chmod 400 bastion_key.pem
 
 scp -o StrictHostKeyChecking=no -i bastion_key.pem ec2_key.pem ubuntu@$(terraform output bastion_host_public_ip | tr -d '"'):~/
 
-echo "Wait for 6-7 minutes while infra deployment is happening. "
 
-sleep 385
 
-echo "Now ssh into bastion, ansible in also running"
+echo "Now ssh into bastion and run the ansible playbook."
 echo "To ssh: ssh -o StrictHostKeyChecking=no -i bastion_key.pem ubuntu@$(terraform output bastion_host_public_ip | tr -d '"') "
 
 echo "To cleanup run: terraform destroy --auto-approve"
-
-
-
-
-
-
-
-
 

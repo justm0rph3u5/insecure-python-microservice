4041258e052de837e74f33646bd9306954ff85d8
DIVYANSHU SHUKLA <86191568+justmorpheus@users.noreply.github.com>
2023-01-16 03:31:17 +0530

    Update pre_init.sh

--- pre_init.sh
+++ pre_init.sh
@@ -26,12 +26,12 @@ echo "Enabling kubectl forward proxy to access internal application. "
 ssh -o ProxyCommand="ssh -i bastion_key.pem -W %h:%p -o StrictHostKeyChecking=no ubuntu@$(terraform output bastion_host_public_ip | tr -d '"')" -o StrictHostKeyChecking=no -i ec2_key.pem ubuntu@$(terraform output private_ec2_private_ip_slave1 | tr -d '"') "nohup kubectl port-forward svc/microservice1 8080:8080 --address='0.0.0.0' >/dev/null 2>&1 &"
 
 echo "Enabling kubectl forward proxy to access kubernetes Dashboard. "
-ssh -o ProxyCommand="ssh -i bastion_key.pem -W %h:%p -o StrictHostKeyChecking=no ubuntu@$(terraform output bastion_host_public_ip | tr -d '"')" -o StrictHostKeyChecking=no -i ec2_key.pem ubuntu@$(terraform output private_ec2_private_ip_slave3 | tr -d '"') "nohup kubectl port-forward -n kubernetes-dashboard service/kubernetes-dashboard 9999:443 --address='0.0.0.0' >/dev/null 2>&1 &"
+ssh -o ProxyCommand="ssh -i bastion_key.pem -W %h:%p -o StrictHostKeyChecking=no ubuntu@$(terraform output bastion_host_public_ip | tr -d '"')" -o StrictHostKeyChecking=no -i ec2_key.pem ubuntu@$(terraform output private_ec2_private_ip_slave3 | tr -d '"') "nohup kubectl port-forward -n kubernetes-dashboard service/kubernetes-dashboard 30033:443 --address='0.0.0.0' >/dev/null 2>&1 &"
 echo "Enabling dynamic application level port forwarding."
 ssh -D 9090 -f -C -q -N -i bastion_key.pem -o StrictHostKeyChecking=no ubuntu@$(terraform output bastion_host_public_ip | tr -d '"')
 
 echo "Now enable socks proxy in the browser and forward to localhost:9090, use foxyproxy to access the internal application at $(terraform output private_ec2_private_ip_slave1 | tr -d '"'):8080"
-echo "Access kubernetes Dashboard at $(terraform output private_ec2_private_ip_slave1 | tr -d '"'):9999"
+echo "Access kubernetes Dashboard at $(terraform output private_ec2_private_ip_slave1 | tr -d '"'):30033"
 
 echo "To cleanup run: terraform destroy --auto-approve"
 

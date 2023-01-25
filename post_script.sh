928efc38b85a3097cffba063eb5ee3e975194d37
DIVYANSHU SHUKLA <86191568+justmorpheus@users.noreply.github.com>
2023-01-25 05:50:09 +0530

    Update post_script.sh

--- post_script.sh
+++ post_script.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
+echo "###############################################################################"
+echo "#                             KubeKrack                                       #"
+echo "#                  Vulnerable Kubernetes Demo Lab                             #"
+echo "#                                                                             #"
+echo "#                           Deletion Script                                   #"
+echo "#                                                                             #"
+echo "#                  Thank you for using KubeKrack Lab                          #"
+echo "#                                                                             #"
+echo "###############################################################################"
+
+# Disable the socks proxy
+unset http_proxy
+unset https_proxy
+
+# Change directory to infrastructure/terraform
 alias cd_temp="cd infrastructure/terraform"
 cd_temp
 
+# Run terraform destroy command
 terraform destroy --auto-approve
-echo "Terraform destroy complete"
 
+# change the directory back to the main
 alias cd_temp="cd ../../"
 cd_temp
 
+echo "###############################################################################"
+echo "# Terraform destroy complete                                                  #"
+echo "###############################################################################"
+# exit the script with a proper status code
+exit 0
